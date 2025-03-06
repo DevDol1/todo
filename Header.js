16a53c8302e038233803927f2d85146dc6924c26
DevDol1 <92390959+DevDol1@users.noreply.github.com>
2025-03-06 02:26:35 +0200

    Create Header.js

---
+++ Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="todoHeader">
+      <h2>To-Do List</h2>
+      <img src="/images/checklist.webp" alt="logo" width="60" height="60" />
+    </div>
+  );
+}
+
+export default Header;
